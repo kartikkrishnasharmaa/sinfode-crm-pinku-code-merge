b17f11e6e07c396dcf542758656174b0befbbbfb
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2025-10-27 18:03:47 +0530

    update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://crmm.sinfode.com/api/',
-  // baseURL: 'https://crmtesting.sinfode.com/api/',
+  // baseURL: 'https://crmtesting.sinfode.com/api//',
 
   headers: {
     'Content-Type': 'application/json',
